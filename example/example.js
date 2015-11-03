99afea36b9149d0183d2a6603967786d37932479
Chris Hitchcott <hitchcott@gmail.com>
2015-11-03 11:03:21 +0800

    remove `autopublish` and `insecure` from example

--- example/example.js
+++ example/example.js
@@ -1,3 +1,12 @@
 if(Meteor.isClient){
-  Template.hello.helpers({'imageData' : EZWebApp.imageData})
+  Template.hello.helpers({'imageData' : EZWebApp.imageData});
+}
+
+if(Meteor.isServer){
+  EZWebApp.collection.allow({
+    insert : function() {return true},
+    update : function() {return true},
+    remove : function() {return true},
+    download : function() {return true}
+  })
 }
